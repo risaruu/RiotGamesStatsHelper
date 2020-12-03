12f3d7c8427d0d78652f9e79c47e5e13123b56ae
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-03 19:36:35 +0100

    updated winrate function and added variable function
    updated winrate function to only show 2 decimals and planing to put displayed data into global variables

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import requests
 apiKeyFile = open('API_Key.txt')
 apiKey = apiKeyFile.read()
 
+#Variablen
+name = "default_Name"
+queueType = "default_queue_type"
+wins = 0
+losses = 0
+lvl = 0
+tier = "hiniger"
+rank = "rank"
+
 #Function to get the basic summoner data of a given summoner
 def getSummoner(summonerName):
     url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + apiKey
@@ -18,22 +27,26 @@ def getRankedStats(summonerName):
     response = requests.get(url)
     return response.json()
 
-
-
 def calculateWinrate(x, y):
     z = x + y
-    return x / z * 100
+    z_formatted = round(x / z * 100, 2)
+    return str(z_formatted)
+
+def variablenSetzen():
+    pass
 
 if __name__ == '__main__':
     #Printing out a welcoming message
     print('Welcome to the RiotGamesHelper by Damjan Petrovic!')
 
-    summonerName = input('Gib eine den Summoner Name: ')
+    #summonerName = input('Gib eine den Summoner Name: ')
+    summonerName = "athelesia"
     summonerData = getSummoner(summonerName)
     summonerDataRanked = getRankedStats(summonerName)
 
     print(summonerData["id"])
     print(summonerDataRanked[1]["queueType"])
     print(summonerData["name"] + " ist gerade Lv." + str(summonerData["summonerLevel"]))
+    print(summonerDataRanked[1]["queueType"])
     print("Tier: " + summonerDataRanked[1]["tier"] + " " + summonerDataRanked[1]["rank"])
-    print("Seine Winrate in" + calculateWinrate(summonerDataRanked[1]["wins"], summonerDataRanked[1]["losses"]))
+    print("Seine Winrate beträgt: " + calculateWinrate(summonerDataRanked[1]["wins"], summonerDataRanked[1]["losses"]))
