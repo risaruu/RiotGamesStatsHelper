9591e8a7eea690d8fdf3c38cb585b8d1c6d36b80
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-03 17:49:44 +0100

    Defining the first two functions and testing commiting from pycharm

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import requests
+import json
+
+#setting the API Key so you dont need to manually type it all the time
+apiKey = ''
+
+#Function to get the basic summoner data of a given summoner
+def getSummoner(summonerName):
+    pass
+
+#Function to get the specific ranked data of a given summoner
+def getRankedStats(summonerName):
+    pass
+
 if __name__ == '__main__':
     print('Welcome to the RiotGamesHelper by Damjan Petrovic!')
 
