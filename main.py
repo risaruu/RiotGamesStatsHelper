51b6b64f21e0de93502f71a6255aa133f3bd3fb3
Damjan Petrovic <p.damjan7999@gmail.com>
2020-12-03 17:53:44 +0100

    Adding a text file that contains our API Key, reading it and printing out the test key to see if it works along with the type to confirm its a string

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import json
 
 #setting the API Key so you dont need to manually type it all the time
-apiKey = ''
+apiKeyFile = open('API_Key.txt')
+apiKey = apiKeyFile.read()
 
 #Function to get the basic summoner data of a given summoner
 def getSummoner(summonerName):
@@ -14,4 +15,6 @@ def getRankedStats(summonerName):
 
 if __name__ == '__main__':
     print('Welcome to the RiotGamesHelper by Damjan Petrovic!')
+    print(apiKey)
+    print(type(apiKey))
 
